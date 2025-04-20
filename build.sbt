03c03b866efb6e3de578dbec4f577affd42ceb31
KVSemionov <kv.semionov@ya.ru>
2025-04-20 10:44:55 +0300

    Fix tests

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "LahNumbers"
 version := "0.1"
 
 scalaVersion := "2.13.13"
+
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % Test
--- src/test/scala/LahNumbersTest.scala
+++ src/test/scala/LahNumbersTest.scala
@@ -1,15 +1,15 @@
 import org.scalatest.funsuite.AnyFunSuite
 
 class LahNumbersTest extends AnyFunSuite {
-  test("lah(5, 2) == 120") {
-    assert(LahNumbers.lah(5, 2) == 120)
+  test("lah(5, 2) == 240") {
+    assert(LahNumbers.lah(5, 2) == 240)
   }
 
-  test("lah(0, 0) == 0") {
-    assert(LahNumbers.lah(0, 0) == 0)
+  test("lah(6, 4) == 300") {
+    assert(LahNumbers.lah(6, 4) == 300)
   }
 
-  test("lah(3, 4) == 0") {
-    assert(LahNumbers.lah(3, 4) == 0)
+  test("lah(8, 2) == 141120") {
+    assert(LahNumbers.lah(8, 2) == 141120)
   }
 }
